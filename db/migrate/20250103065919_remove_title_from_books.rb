2ea6ee0a6158011db445747a0ece0983e86554f0
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-01-03 07:24:27 +0000

    [Add]kadai1-5

--- db/migrate/20250103065919_remove_title_from_books.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTitleFromBooks < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :books, :title, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_01_03_065919) do
+ActiveRecord::Schema.define(version: 2025_01_03_072237) do
 
   create_table "books", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
   end
 
 end
